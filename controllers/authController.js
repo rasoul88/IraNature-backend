3c50caebbe235113671f3e9ccdc353aa29c3d644
sahraeirasoul <sahraeirasoul@gmail.com>
2021-11-28 20:05:28 +0330

    I changed sameSite to 'none' in res.cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,11 +19,9 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: false,
-    sameSite: 'none',
-    secure: true
+    httpOnly: true
   };
-  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
   // Remove password from output
