678879c3abd97a253e599a8e6efc89185dcec84c
sahraeirasoul <sahraeirasoul@gmail.com>
2021-11-28 22:06:40 +0330

    I changed sameSite to 'none' in res.cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,8 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    sameSite: 'none'
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
