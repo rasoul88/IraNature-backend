5d39ab9bec4f84abd363d91ff3e9a5cc8bdcf785
sahraeirasoul <sahraeirasoul@gmail.com>
2021-12-05 11:06:05 +0330

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,9 +19,9 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
     // when you use sameSite your client should be https://
-    // sameSite: 'none'
+    sameSite: 'none'
   };
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
