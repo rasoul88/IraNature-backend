f86d1e29636e1c8ac46373420f338075a36a9feb
sahraeirasoul <sahraeirasoul@gmail.com>
2021-12-05 11:47:19 +0330

    some small modification

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://irnature-9h8erpxss-rasoul88.vercel.app'
+    'https://irnature-9h8erpxss-rasoul88.vercel.app',
+    'https://irnature.vercel.app'
   ],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200
